21607097e97026c054dc50a85b6bb1cd0d8d4980
omerincirkus <72596679+omerincirkus@users.noreply.github.com>
2021-08-27 20:55:38 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
-    <div className='App'>
-      <h1>Ömer İncirkuşsada</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact />
+      </Switch>
+    </Router>
   );
 }
 
